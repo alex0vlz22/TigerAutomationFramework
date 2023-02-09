1c3956142ac24c0731c662285151bf751037137e
One Developer <onedeveloper@gmail.com>
2023-02-09 12:55:15 -0500

    Framework has been created!

---
+++ src/main/java/pages/base/BasePage.java
@@ -0,0 +1,11 @@
+package pages.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.webdriver.WebDriverBrowser;
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+
+
+
+}
